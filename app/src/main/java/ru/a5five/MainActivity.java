2fb4d1a566f207bffb279962f2ef2f0c47811417
Nellrun <root@mail.nellrun.com>
2016-10-20 22:34:47 +0300

    added user list

--- app/src/main/java/ru/a5five/MainActivity.java
+++ app/src/main/java/ru/a5five/MainActivity.java
@@ -6,8 +6,15 @@ import android.support.v7.view.ActionMode;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,7 +26,7 @@ import retrofit2.http.GET;
 public class MainActivity extends AppCompatActivity {
 
     public interface GetUsersInterface {
-        @GET("data.json")
+        @GET("https://agency5-mobile-school.firebaseio.com/data.json")
         Call<Users> getUsers();
     }
 
@@ -30,11 +37,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lv = (ListView) findViewById(R.id.listView);
-
-        Button[] b = {new Button(this), new Button(this)};
-        ArrayAdapter<Button> aB = new ArrayAdapter<Button>(this, R.layout.support_simple_spinner_dropdown_item, b);
-        lv.setAdapter(aB);
+//        ListView lv = (ListView) findViewById(R.id.listView);
+//
+//        Button[] b = {new Button(this), new Button(this)};
+//        ArrayAdapter<Button> aB = new ArrayAdapter<Button>(this, R.layout.support_simple_spinner_dropdown_item, b);
+//        lv.setAdapter(aB);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -44,20 +51,37 @@ public class MainActivity extends AppCompatActivity {
         GetUsersInterface g = retrofit.create(GetUsersInterface.class);
         Call<Users> users = g.getUsers();
 
+
+        Toast.makeText(getApplicationContext(), "Получение списка...", Toast.LENGTH_LONG).show();
+
         users.enqueue(new Callback<Users>() {
 
-            @Override
+                @Override
             public void onResponse(Call<Users> call, Response<Users> response) {
-                try {
-
-                    Toast.makeText(getApplicationContext(),response.body().getItems().get(0).getAbout(), Toast.LENGTH_LONG);
-                } catch (Exception e) {
-
-                }
                 if (response.isSuccessful()) {
-                    Toast.makeText(getApplicationContext(),"Данные успешно загружены", Toast.LENGTH_LONG);
+                    Toast.makeText(getApplicationContext(),"Данные успешно загружены", Toast.LENGTH_LONG).show();
+
+                    ListView lv = (ListView) findViewById(R.id.userListView);
+
+                    ArrayList<HashMap<String, Object>> al = new ArrayList<HashMap<String, Object>>();
+
+                    HashMap<String, Object> elem;
+                    for (Item human: response.body().getItems()) {
+//                        s.add(human.getFirstName() + " " + human.getLastName());
+                        elem = new HashMap<String, Object>();
+                        elem.put("img", R.mipmap.ic_launcher);
+                        elem.put("name", human.getFirstName() + " " + human.getLastName());
+                        al.add(elem);
+                    }
+
+                    SimpleAdapter sa = new SimpleAdapter(getApplicationContext(), al, R.layout.row,
+                            new String[] {"img", "name"},
+                            new int[] {R.id.userImg, R.id.userName});
+
+                    lv.setAdapter(sa);
+
                 } else {
-                    Toast.makeText(getApplicationContext(),"Не удалось загрузить список", Toast.LENGTH_LONG);
+                    Toast.makeText(getApplicationContext(),"Не удалось загрузить список", Toast.LENGTH_LONG).show();
                 }
             }
 
